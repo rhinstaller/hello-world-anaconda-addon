b4be79f7e1a6e8721e647fb2e8011487637993b7
Vratislav Podzimek <vpodzime@redhat.com>
2013-02-05 22:39:12 +0100

    Hello world spoke
    An example of a spoke that will be included in the Hello world category
    and thus on the Summary hub.

---
+++ org_fedora_hello_world/gui/spokes/hello_world.py
@@ -0,0 +1,213 @@
+#
+# Copyright (C) 2013  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+#
+# Red Hat Author(s): Vratislav Podzimek <vpodzime@redhat.com>
+#
+
+"""Module with the HelloWorldSpoke class."""
+
+# import gettext
+# _ = lambda x: gettext.ldgettext("hello-world-anaconda-plugin", x)
+
+# will never be translated
+_ = lambda x: x
+N_ = lambda x: x
+
+from ..categories.hello_world import HelloWorldCategory
+from pyanaconda.ui.gui import GUIObject
+from pyanaconda.ui.gui.spokes import NormalSpoke
+from pyanaconda.ui.gui.utils import enlightbox
+
+# export only the spoke, no helper functions, classes or constants
+__all__ = ["HelloWorldSpoke"]
+
+class HelloWorldSpoke(NormalSpoke):
+    """
+    Class for the Hello world spoke. This spoke will be in the Hello world
+    category and thus on the Summary hub. It is a very simple example of
+    a unit for the Anaconda's graphical user interface.
+
+    @see: pyanaconda.ui.common.UIObject
+    @see: pyanaconda.ui.common.Spoke
+    @see: pyanaconda.ui.gui.GUIObject
+
+    """
+
+    ### class attributes defined by API ###
+
+    # list all top-level objects from the .glade file that should be exposed
+    # to the spoke or leave empty to extract everything
+    builderObjects = ["helloWorldSpokeWindow", "buttonImage"]
+
+    # the name of the main window widget
+    mainWidgetName = "helloWorldSpokeWindow"
+
+    # name of the .glade file
+    # TODO: how are these paths handled?
+    uiFile = "hello_world.glade"
+
+    # category this spoke belongs to
+    category = HelloWorldCategory
+
+    # spoke icon (will be displayed on the hub)
+    # preferred are the -symbolic icons as these are used in Anaconda's spokes
+    icon = "face-cool-symbolic"
+
+    # title of the spoke (will be displayed on the hub)
+    title = N_("_HELLO WORLD")
+
+    ### methods defined by API ###
+    def __init__(self, data, storage, payload, instclass):
+        """
+        @see: pyanaconda.ui.common.Spoke.__init__
+        @param data: data object passed to every spoke to load/store data
+                     from/to it
+        @type data: pykickstart.base.BaseHandler
+        @param storage: object storing storage-related information
+                        (disks, partitioning, bootloader, etc.)
+        @type storage: blivet.Storage
+        @param payload: object storing packaging-related information
+        @type payload: pyanaconda.packaging.Payload
+        @param instclass: distribution-specific information
+        @type instclass: pyanaconda.installclass.BaseInstallClass
+
+        """
+
+        NormalSpoke.__init__(self, data, storage, payload, instclass)
+
+    def initialize(self):
+        """
+        The initialize method that is called after the instance is created.
+        The difference between __init__ and this method is that this may take
+        a long time and thus could be called in a separated thread.
+
+        @see: pyanaconda.ui.common.UIObject.initialize
+
+        """
+
+        NormalSpoke.initialize(self)
+        self._entry = self.builder.get_object("textEntry")
+
+    def refresh(self):
+        """
+        The refresh method that is called every time the spoke is displayed.
+        It should update the UI elements according to the contents of
+        self.data.
+
+        @see: pyanaconda.ui.common.UIObject.refresh
+
+        """
+
+        self._entry.set_text(self.data.addons.org_fedora_hello_world.text)
+
+    def apply(self):
+        """
+        The apply method that is called when the spoke is left. It should
+        update the contents of self.data with values set in the GUI elements.
+
+        """
+
+        self.data.org_fedora_hello_world.text = self._entry.get_text()
+
+    def execute(self):
+        """
+        The excecute method that is called when the spoke is left. It is
+        supposed to do all changes to the runtime environment according to
+        the values set in the GUI elements.
+
+        """
+
+        # nothing to do here
+        pass
+
+    @property
+    def completed(self):
+        """
+        The completed property that tells whether all mandatory items on the
+        spoke are set, or not. The spoke will be marked on the hub as completed
+        or uncompleted acording to the returned value.
+
+        @rtype: bool
+
+        """
+
+        return bool(self._entry.get_text())
+
+    @property
+    def status(self):
+        """
+        The status property that is a brief string describing the state of the
+        spoke. It should describe whether all values are set and if possible
+        also the values themselves. The returned value will appear on the hub
+        below the spoke's title.
+
+        """
+
+        text = self._entry.get_text()
+        if text:
+            return _("Text set: %s") % text
+        else:
+            return _("Text not set")
+
+
+    ### handlers ###
+    def on_entry_icon_clicked(self, entry, *args):
+        """Handler for the textEntry's "icon-release" signal."""
+
+        entry.set_text("")
+
+    def on_main_button_clicked(self, *args):
+        """Handler for the mainButton's "clicked" signal."""
+
+        dialog = HelloWorldDialog()
+
+        # show dialog above the lightbox
+        with enlightbox(self.window, dialog.window):
+            dialog.run()
+
+class HelloWorldDialog(GUIObject):
+    """
+    Class for the sample dialog.
+
+    @see: pyanaconda.ui.common.UIObject
+    @see: pyanaconda.ui.gui.GUIObject
+
+    """
+
+    builderObjects = ["sampleDialog"]
+    mainWidgetName = "sampleDialog"
+    uiFile = "hello_world.glade"
+
+    def __init__(self, *args):
+        GUIObject.__init__(self, *args)
+
+    def initialize(self):
+        GUIObject.initialize(self)
+
+    def run(self):
+        """
+        Run dialog and destroy its window.
+
+        @returns: respond id
+        @rtype: int
+
+        """
+
+        ret = self.window.run()
+        self.window.destroy()
+
+        return ret
