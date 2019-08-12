8404d91cb20ca6ad0620a8dde0ba849a9a647d04
Sanjit Chakrabarti <sujitsanjit@gmail.com>
2019-08-12 18:32:44 +0530

    Removed instclass from ks

--- org_fedora_hello_world/ks/hello_world.py
+++ org_fedora_hello_world/ks/hello_world.py
@@ -132,7 +132,7 @@ class HelloWorldData(AddonData):
         # no actions needed in this addon
         pass
 
-    def setup(self, storage, ksdata, instclass, payload):
+    def setup(self, storage, ksdata, payload):
         """
         The setup method that should make changes to the runtime environment
         according to the data stored in this object.
@@ -154,7 +154,7 @@ class HelloWorldData(AddonData):
         # no actions needed in this addon
         pass
 
-    def execute(self, storage, ksdata, instclass, users, payload):
+    def execute(self, storage, ksdata, users, payload):
         """
         The execute method that should make changes to the installed system. It
         is called only once in the post-install setup phase.
